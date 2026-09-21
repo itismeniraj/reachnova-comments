f827377d545fb04531c78a41b831cf7ccff20d59
Niraj <itismeniraj@gmail.com>
2026-09-21 12:46:19 +0545

    counter added for max characters

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 const FeedbackForm = () => {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
   return (
     <form className="form">
-      <textarea spellCheck={false} id="feedback-textarea" placeholder="" />
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        spellCheck={false}
+        id="feedback-textarea"
+        placeholder=""
+      />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the feedback
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
