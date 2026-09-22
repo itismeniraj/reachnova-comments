890a4403f7e86bd5477acc679856431cd02acab8
Niraj <itismeniraj@gmail.com>
2026-09-22 13:03:28 +0545

    open and close list

--- src/components/FeedbackItem.tsx
+++ src/components/FeedbackItem.tsx
@@ -1,13 +1,18 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import type { FeedbackItemT } from "../types";
+import { useState } from "react";
 
 type FeedbackItemProps = {
   feedbackItem: FeedbackItemT;
 };
 
 const FeedbackItem = ({ feedbackItem }: FeedbackItemProps) => {
+  const [open, setOpen] = useState(false);
   return (
-    <li className="feedback">
+    <li
+      onClick={() => setOpen((prev) => !prev)}
+      className={`feedback ${open ? "feedback--expand" : ""}`}
+    >
       <button>
         <TriangleUpIcon />
         <span>{feedbackItem.upvoteCount}</span>
